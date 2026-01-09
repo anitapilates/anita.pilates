402a94008361e2664f99ef66e83885b309f53f04
anitapilates <anita.pilatess@gmail.com>
2026-01-09 12:41:18 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,11 +19,16 @@ export const metadata: Metadata = {
   title: "Anita Pilates | Instructora de Pilates en Barcelona",
   description: "Pilates Mat y Pilates Reformer en Barcelona",
   icons: {
-    icon: "/favicon-32x32.png",
-    shortcut: "/favicon-16x16.png",
-    apple: "/favicon-16x16.png",
-  },
-  manifest: "/site.webmanifest",
+  icon: [
+    { url: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
+    { url: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
+    { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+  ],
+  apple: "/apple-touch-icon.png",
+},
+manifest: "/site.webmanifest",
+
 };
 
 export default function RootLayout({
